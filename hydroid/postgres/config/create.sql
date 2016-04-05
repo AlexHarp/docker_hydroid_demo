ce36369cea8ec47709c678000105b691938aa586
Darren Reid <darren.reid@ga.gov.au>
2016-04-05 16:56:12 +1000

    Init postgres

---
+++ hydroid/postgres/config/create.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE hydroid;
+CREATE SCHEMA IF NOT EXISTS hydroid AUTHORIZATION hydroid;
+
+
+CREATE TABLE hydroid.documents (
+    id bigserial PRIMARY KEY,
+    origin varchar(500) NOT NULL,
+    urn varchar(100) NULL,
+    title varchar(200) NULL,
+    type varchar(20) NOT NULL,
+    status varchar(20) NOT NULL,
+    status_reason varchar(500) NULL,
+    process_date timestamp NOT NULL
+
+);
+
+CREATE UNIQUE INDEX documents_urn_idx ON hydroid.documents (urn);
+CREATE UNIQUE INDEX documents_origin_idx ON hydroid.documents (origin);
+
+CREATE TABLE hydroid.image_metadata (
+    origin varchar(500) PRIMARY KEY,
+    metadata varchar(1000) NOT NULL
+);
