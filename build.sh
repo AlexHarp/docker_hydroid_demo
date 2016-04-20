21cda6f713b45c4c1767d5708c29c460d2898915
Darren Reid <darren.reid@ga.gov.au>
2016-04-20 15:39:52 +1000

    Change startup

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ mkdir -p ./hydroid/api/src/
 git clone https://github.com/GeoscienceAustralia/hydroid.git ./hydroid/api/src/
 mkdir -p ./hydroid/web/src/
 git clone https://github.com/GeoscienceAustralia/hydroid-client.git ./hydroid/web/src/
-docker-compose build
+docker-compose up -d
+docker exec dockerhydroiddemo_stanbol_1 /var/tmp/post-startup.sh
---
+++ hydroid/stanbol/vocab/post-startup.sh
@@ -0,0 +1,3 @@
+cp /var/tmp/indexing/dist/GA.solrindex.zip /usr/local/tomcat/stanbol/datafiles/GA.solrindex.zip
+cp /var/tmp/indexing/dist/org.apache.stanbol.data.site.GA-1.0.0.jar /usr/local/tomcat/stanbol/fileinstall/org.apache.stanbol.data.site.GA-1.0.0.jar
+cp /var/tmp/config/. /usr/share/tomcat7/stanbol/fileinstall/ -R
